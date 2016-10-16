67519684218bf6102a4b80503577071c295aaa7e
joevansanttos <joevansanttos@gmail.com>
2016-10-16 10:53:52 -0300

    Home

--- app/views/home.php
+++ app/views/home.php
@@ -5,26 +5,35 @@ include_once "header.php"
 
 
 <div class="row containe-solid">
-  <h4>Meus Trabalhos   <button type="button" class="btn btn-default pull-right"  value="Input Button" >Adicionar Trabalho</button></h4>
+  <h4>Meus Trabalhos   <button type="button" class="btn btn-default pull-right"  value="Input Button" >Adicionar Trabalho</button></h4>  
   <div class="row">
     <div class="col-sm-4">
-      <div class="panel">
+      <div class="panel panel-primary">
         <div class="panel-heading">Postagem 1</div>
         <div class="panel-body"><small>Algum texto qualquer</small></div>
+        <div style="text-align: center;">
+          <input type="button" value="button">
+        </div>
         <div class="panel-footer"><small>Comentarios</small></div>
       </div>
     </div>
     <div class="col-sm-4">
-      <div class="panel">
+      <div class="panel panel-primary">
         <div class="panel-heading">Postagem 2</div>
-        <div class="panel-body"></div>
+        <div class="panel-body"><small>Algum texto qualquer</small></div>
+        <div style="text-align: center;">
+          <input type="button" value="button">
+        </div>
         <div class="panel-footer"><small>Comentarios</small></div>
       </div>
     </div>
     <div class="col-sm-4">
-      <div class="panel">
+      <div class="panel  panel-primary">
         <div class="panel-heading">Postagem 3</div>
-        <div class="panel-body"></div>
+        <div class="panel-body"><small>Algum texto qualquer</small></div>
+        <div style="text-align: center;">
+          <input type="button" value="button">
+        </div>
         <div class="panel-footer"><small>Comentarios</small></div>
       </div>
     </div>
@@ -35,45 +44,62 @@ include_once "header.php"
   <h4>Adicionados Recentemente</h4>
   <div class="row">
     <div class="col-sm-4">
-      <div class="panel">
+      <div class="panel  panel-primary">
         <div class="panel-heading">Postagem 1</div>
-
-        <div class="panel-body"></div>
+        <div class="panel-body"><small>Algum texto qualquer</small></div>
+        <div style="text-align: center;">
+          <input type="button" value="button">
+        </div>
         <div class="panel-footer"><small>Comentarios</small></div>
       </div>
     </div>
     <div class="col-sm-4">
-      <div class="panel">
+      <div class="panel  panel-primary">
         <div class="panel-heading">Postagem 2</div>
-        <div class="panel-body"></div>
+        <div class="panel-body"><small>Algum texto qualquer</small></div>
+        <div style="text-align: center;">
+          <input type="button" value="button">
+        </div>
         <div class="panel-footer"><small>Comentarios</small></div>
       </div>
     </div>
-    <div class="col-sm-4">
-      <div class="panel">
+    <div class="col-sm-4 ">
+      <div class="panel  panel-primary">
         <div class="panel-heading">Postagem 3</div>
-        <div class="panel-body"></div>
+        <div class="panel-body"><small>Algum texto qualquer</small></div>
+        <div style="text-align: center;">
+          <input type="button" value="button">
+        </div>
         <div class="panel-footer"><small>Comentarios</small></div>
       </div>
     </div>
     <div class="col-sm-4">
-      <div class="panel">
+      <div class="panel  panel-primary">
         <div class="panel-heading">Postagem 4</div>
-        <div class="panel-body"></div>
+        <div class="panel-body"><small>Algum texto qualquer</small></div>
+        <div style="text-align: center;">
+          <input type="button" value="button">
+        </div>
         <div class="panel-footer"><small>Comentarios</small></div>
       </div>
     </div>
     <div class="col-sm-4">
-      <div class="panel">
+      <div class="panel  panel-primary">
         <div class="panel-heading">Postagem 5</div>
-        <div class="panel-body"></div>
+        <div class="panel-body"><small>Algum texto qualquer</small></div>
+        <div style="text-align: center;">
+          <input type="button" value="button">
+        </div>
         <div class="panel-footer"><small>Comentarios</small></div>
       </div>
     </div>
     <div class="col-sm-4">
-      <div class="panel">
+      <div class="panel  panel-primary">
         <div class="panel-heading">Postagem 6</div>
-        <div class="panel-body"></div>
+        <div class="panel-body"><small>Algum texto qualquer</small></div>
+        <div style="text-align: center;">
+          <input type="button" value="button">
+        </div>
         <div class="panel-footer"><small>Comentarios</small></div>
       </div>
     </div>
