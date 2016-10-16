bcf95759f4d2403e3d1fc5a486c59a02db935f6a
joevansanttos <joevansanttos@gmail.com>
2016-10-16 10:01:05 -0300

    Home
    Home da aplicação

---
+++ app/views/home.php
@@ -0,0 +1,54 @@
+<?php
+include_once "header.php"
+?>
+
+
+
+<div class="row container-fluid">
+  <h4>Meus Trabalhos </h4>
+  <button type="button" class="btn btn-default pull-right"  value="Input Button" >Adicionar Trabalho</button>
+  <div class="container">
+ <div class="row">
+     <div class="col-lg-12">
+         <div class="panel panel-default">
+             <div class="panel-heading-fd">
+                 <h3 class="panel-title-fd">Comments</h3>
+             </div>
+
+             <div class="panel-body">
+                 <div class="media">
+       <a class="pull-left" href="#">
+         <img class="media-object" data-src="holder.js/64x64" alt="64x64" src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSI2NCIgaGVpZ2h0PSI2NCI+PHJlY3Qgd2lkdGg9IjY0IiBoZWlnaHQ9IjY0IiBmaWxsPSIjZWVlIi8+PHRleHQgdGV4dC1hbmNob3I9Im1pZGRsZSIgeD0iMzIiIHk9IjMyIiBzdHlsZT0iZmlsbDojYWFhO2ZvbnQtd2VpZ2h0OmJvbGQ7Zm9udC1zaXplOjEycHg7Zm9udC1mYW1pbHk6QXJpYWwsSGVsdmV0aWNhLHNhbnMtc2VyaWY7ZG9taW5hbnQtYmFzZWxpbmU6Y2VudHJhbCI+NjR4NjQ8L3RleHQ+PC9zdmc+" style="width: 64px; height: 64px;">
+       </a>
+       <div class="media-body">
+         <h4 class="media-heading"><a href="https://www.facebook.com/FadselTechnologies">Fadsel Technologies</a></h4>
+         Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+       </div>
+     </div>
+
+             </div>
+         </div>
+     </div>
+
+ </div>
+ </div>
+ <div class="col-sm-4">Minha Postagem 2</div>
+ <div class="col-sm-4">Minha Postagem 3</div>
+</div>
+<br></br>
+
+<div class="row container-fluid" >
+  <h4>Adicionados Recentemente </h4>
+ <div class="col-sm-4">Sua Postagem 1</div>
+ <div class="col-sm-4">Sua Postagem 2</div>
+ <div class="col-sm-4">Sua Postagem 3</div>
+ <div class="col-sm-4">Sua Postagem 4</div>
+ <div class="col-sm-4">Sua Postagem 5</div>
+ <div class="col-sm-4">Sua Postagem 6</div>
+</div>
+
+
+
+<?php
+include_once "footer.php"
+?>
