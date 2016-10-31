b26227957b50e173fac7409e950ea15e1eda7971
joevansanttos <joevansanttos@gmail.com>
2016-10-31 01:31:16 -0300

    vertodos
    Pagina ver Todos

---
+++ app/views/header2.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+        <title>Moutain</title>
+        <link rel="stylesheet" href="..\vendors\bootstrap\css\bootstrap.min.css" type="text/css" />
+        <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/jasny-bootstrap/3.1.3/css/jasny-bootstrap.min.css">
+        <!--[if IE]>
+          <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+        <!--[if lte IE 7]>
+          <script src="js/IE8.js" type="text/javascript"></script><![endif]-->
+        <!--[if lt IE 7]>
+          <link rel="stylesheet" type="text/css" media="all" href="css/ie6.css"/><![endif]-->
+    </head>
+<body id="index" class="home">
+        <nav class="navbar navbar-default">
+      <div class="container-fluid">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#"><img style="height: 110px; margin-top: -35px" src="..\assets\img\logo.png" alt="" /></a>
+        </div>
+
+        <div class="dropdown">
+          <div class="col-lg-12">
+            <div class="text-right ">
+              <button type="button center" class="btn btn-default ">Sair</button>
+            </div>
+        </div>
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+
+        </div><!-- /.navbar-collapse -->
+      </div><!-- /.container-fluid -->
+    </nav>
+    <div class="container">
---
+++ app/views/verTodos.php
@@ -0,0 +1,86 @@
+<?php
+include_once "header2.php"
+?>
+
+<div class="row containe-solid">
+  <h4>Trabalhos</h4>
+     <div class="span12">
+          <form id="custom-search-form" class="form-search form-horizontal pull-right">
+             <div class="input-append span12">
+                  <input type="text" class="search-query" placeholder="Search">
+                  <button type="submit" class="btn"><i class="icon-search"></i></button>
+             </div>
+           </form>
+      </div>
+</div>
+  <br></br>
+  <div class="row">
+    <div class="col-sm-4">
+      <div class="panel panel-primary">
+        <div class="panel-heading">Postagem 1</div>
+        <div class="panel-body"><small>Algum texto qualquer</small></div>
+        <div style="text-align: center;">
+          <input type="button" value="button">
+        </div>
+        <div class="panel-footer"><a href="#">Comentarios</a></li></div>
+      </div>
+    </div>
+    <div class="col-sm-4">
+      <div class="panel panel-primary">
+        <div class="panel-heading">Postagem 2</div>
+        <div class="panel-body"><small>Algum texto qualquer</small></div>
+        <div style="text-align: center;">
+          <input type="button" value="button">
+        </div>
+        <div class="panel-footer"><a href="#">Comentarios</a></div>
+      </div>
+    </div>
+    <div class="col-sm-4">
+      <div class="panel  panel-primary">
+        <div class="panel-heading">Postagem 3</div>
+        <div class="panel-body"><small>Algum texto qualquer</small></div>
+        <div style="text-align: center;">
+          <input type="button" value="button">
+        </div>
+        <div class="panel-footer"><a href="#">Comentarios</a></div>
+      </div>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-sm-4">
+      <div class="panel panel-primary">
+        <div class="panel-heading">Postagem 4</div>
+        <div class="panel-body"><small>Algum texto qualquer</small></div>
+        <div style="text-align: center;">
+          <input type="button" value="button">
+        </div>
+        <div class="panel-footer"><a href="#">Comentarios</a></li></div>
+      </div>
+    </div>
+    <div class="col-sm-4">
+      <div class="panel panel-primary">
+        <div class="panel-heading">Postagem 5</div>
+        <div class="panel-body"><small>Algum texto qualquer</small></div>
+        <div style="text-align: center;">
+          <input type="button" value="button">
+        </div>
+        <div class="panel-footer"><a href="#">Comentarios</a></div>
+      </div>
+    </div>
+    <div class="col-sm-4">
+      <div class="panel  panel-primary">
+        <div class="panel-heading">Postagem 6</div>
+        <div class="panel-body"><small>Algum texto qualquer</small></div>
+        <div style="text-align: center;">
+          <input type="button" value="button">
+        </div>
+        <div class="panel-footer"><a href="#">Comentarios</a></div>
+      </div>
+    </div>
+  </div>
+<br></br>
+
+
+<?php
+include_once "footer.php"
+?>
